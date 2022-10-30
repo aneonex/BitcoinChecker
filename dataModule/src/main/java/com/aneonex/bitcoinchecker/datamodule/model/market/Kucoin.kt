c977b89962d63ca841fafdf052cb6abb0cf0275d
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2022-10-30 23:49:37 +0300

    KuCoin: API v2 + refactoring

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Kucoin.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Kucoin.kt
@@ -2,51 +2,60 @@ package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
 import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
+import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
 import org.json.JSONObject
 
-class Kucoin : Market(NAME, TTS_NAME, null) {
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyPairId)
-    }
+class Kucoin : SimpleMarket(
+    "KuCoin",
+    "https://api.kucoin.com/api/v2/symbols",
+    "https://api.kucoin.com/api/v1/market/stats?symbol=%1\$s"
+) {
 
     @Throws(Exception::class)
-    override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
-        val tickerJsonObject = jsonObject.getJSONObject("data")
-        ticker.bid = tickerJsonObject.getDouble("buy")
-        ticker.ask = tickerJsonObject.getDouble("sell")
-        ticker.vol = tickerJsonObject.getDouble("vol")
-        ticker.high = tickerJsonObject.getDouble("high")
-        ticker.low = tickerJsonObject.getDouble("low")
-        ticker.last = tickerJsonObject.getDouble("last")
-        ticker.timestamp = tickerJsonObject.getLong("time")
+    override fun parseTickerFromJsonObject(
+        requestId: Int,
+        jsonObject: JSONObject,
+        ticker: Ticker,
+        checkerInfo: CheckerInfo
+    ) {
+        jsonObject.getJSONObject("data").also {
+            ticker.bid = it.getDouble("buy")
+            ticker.ask = it.getDouble("sell")
+
+            ticker.vol = it.getDouble("vol")
+            ticker.volQuote = it.getDouble("volValue")
+
+            ticker.high = it.getDouble("high")
+            ticker.low = it.getDouble("low")
+
+            ticker.last = it.getDouble("last")
+            ticker.timestamp = it.getLong("time")
+        }
     }
 
     // ====================
     // Get currency pairs
     // ====================
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_COINS_PAIRS
-    }
-
     @Throws(Exception::class)
-    override fun parseCurrencyPairsFromJsonObject(requestId: Int, jsonObject: JSONObject, pairs: MutableList<CurrencyPairInfo>) {
-        val data = jsonObject.getJSONArray("data")
-        for (i in 0 until data.length()) {
-            val pairJsonObject = data.getJSONObject(i)
-            pairs.add(CurrencyPairInfo(
-                    pairJsonObject.getString("baseCurrency"),
-                    pairJsonObject.getString("quoteCurrency"),
-                    pairJsonObject.getString("symbol")
-            ))
-        }
-    }
-
-    companion object {
-        private const val NAME = "Kucoin"
-        private const val TTS_NAME = NAME
-        private const val URL = "https://api.kucoin.com/api/v1/market/stats?symbol=%1\$s"
-        private const val URL_COINS_PAIRS = "https://api.kucoin.com/api/v1/symbols"
+    override fun parseCurrencyPairsFromJsonObject(
+        requestId: Int,
+        jsonObject: JSONObject,
+        pairs: MutableList<CurrencyPairInfo>
+    ) {
+        jsonObject
+            .getJSONArray("data")
+            .forEachJSONObject { symbolData ->
+                if (symbolData.getBoolean("enableTrading")) {
+                    pairs.add(
+                        CurrencyPairInfo(
+                            symbolData.getString("baseCurrency"),
+                            symbolData.getString("quoteCurrency"),
+                            symbolData.getString("symbol")
+                        )
+                    )
+                }
+            }
     }
 }
