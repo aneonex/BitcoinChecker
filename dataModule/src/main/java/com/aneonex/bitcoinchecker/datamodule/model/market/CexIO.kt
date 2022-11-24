091f2c9f308823817b2c23f60d0d5193cca69062
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2022-11-24 18:19:39 +0300

    Updated CEX.IO

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/CexIO.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/CexIO.kt
@@ -2,42 +2,22 @@ package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
 import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
+import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
 import org.json.JSONObject
 
-class CexIO : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
-    companion object {
-        private const val NAME = "CEX.IO"
-        private const val TTS_NAME = "CEX IO"
-        private const val URL = "https://cex.io/api/ticker/%1\$s/%2\$s"
-        private const val URL_CURRENCY_PAIRS = "https://cex.io/api/currency_limits"
-        private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
+class CexIO : SimpleMarket(
+    "CEX.IO",
+    "https://cex.io/api/currency_limits",
+    "https://cex.io/api/ticker/%1\$s",
+    "CEX IO"
+) {
 
-        init {
-            CURRENCY_PAIRS[VirtualCurrency.BTC] = arrayOf(
-                    Currency.USD,
-                    Currency.EUR,
-                    Currency.GBP,
-                    Currency.RUB)
-            CURRENCY_PAIRS[VirtualCurrency.ETH] = arrayOf(
-                    Currency.USD,
-                    Currency.EUR,
-                    VirtualCurrency.BTC
-            )
-            CURRENCY_PAIRS[VirtualCurrency.GHS] = arrayOf(
-                    VirtualCurrency.BTC)
-        }
+    override fun getPairId(checkerInfo: CheckerInfo): String {
+        return with(checkerInfo) { "$currencyBase/$currencyCounter" }
     }
 
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyBase, checkerInfo.currencyCounter)
-    }
-
-    @Throws(Exception::class)
     override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
         if (jsonObject.has("bid")) {
             ticker.bid = jsonObject.getDouble("bid")
@@ -51,25 +31,25 @@ class CexIO : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
         ticker.last = jsonObject.getDouble("last")
     }
 
-    // ====================
-    // Get currency pairs
-    // ====================
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_CURRENCY_PAIRS
-    }
-
-    @Throws(Exception::class)
     override fun parseCurrencyPairsFromJsonObject(requestId: Int, jsonObject: JSONObject, pairs: MutableList<CurrencyPairInfo>) {
-        val dataJsonObject = jsonObject.getJSONObject("data")
-        val pairsJsonArray = dataJsonObject.getJSONArray("pairs")
-        for (i in 0 until pairsJsonArray.length()) {
-            val pairJsonObject = pairsJsonArray.getJSONObject(i)
-            val currencyBase = pairJsonObject.getString("symbol1")
-            val currencyCounter = pairJsonObject.getString("symbol2")
-            pairs.add(CurrencyPairInfo(
+        jsonObject
+            .getJSONObject("data")
+            .getJSONArray("pairs")
+            .forEachJSONObject {
+                val currencyBase = it.getString("symbol1")
+                val currencyCounter = it.getString("symbol2")
+                pairs.add(CurrencyPairInfo(
                     currencyBase,
                     currencyCounter,
                     null))
-        }
+            }
+    }
+
+    override fun parseErrorFromJsonObject(
+        requestId: Int,
+        jsonObject: JSONObject,
+        checkerInfo: CheckerInfo?
+    ): String? {
+        return jsonObject.getString("error")
     }
 }
