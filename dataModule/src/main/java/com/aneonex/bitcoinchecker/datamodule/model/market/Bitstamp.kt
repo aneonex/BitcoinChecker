0c4e46ff0d2f0ccd8621aa9a497728ab01d045bb
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2022-11-24 22:20:11 +0300

    Updated Bitstamp

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Bitstamp.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/Bitstamp.kt
@@ -2,105 +2,61 @@ package com.aneonex.bitcoinchecker.datamodule.model.market
 
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
 import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
-import com.aneonex.bitcoinchecker.datamodule.model.currency.Currency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.VirtualCurrency
-import com.aneonex.bitcoinchecker.datamodule.model.currency.CurrencyPairsMap
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
 import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
 import org.json.JSONArray
 import org.json.JSONObject
 
-class Bitstamp : Market(NAME, TTS_NAME, CURRENCY_PAIRS) {
-    companion object {
-        private const val NAME = "Bitstamp"
-        private const val TTS_NAME = NAME
-        private const val URL_TICKER = "https://www.bitstamp.net/api/v2/ticker/%1\$s"
-        private const val URL_CURRENCY_PAIRS = "https://www.bitstamp.net/api/v2/trading-pairs-info/"
+// API: https://www.bitstamp.net/api/
+class Bitstamp : SimpleMarket(
+    "Bitstamp",
+    "https://www.bitstamp.net/api/v2/trading-pairs-info/",
+    "https://www.bitstamp.net/api/v2/ticker/%1\$s"
+) {
 
-        private val CURRENCY_PAIRS: CurrencyPairsMap = CurrencyPairsMap()
+    override fun parseCurrencyPairs(
+        requestId: Int,
+        responseString: String,
+        pairs: MutableList<CurrencyPairInfo>
+    ) {
+        JSONArray(responseString)
+            .forEachJSONObject { market ->
+                if (market.getString("trading") == "Enabled") {
+                    val assetsInPair = market
+                        .getString("name")
+                        .split('/')
 
-        init {
-            CURRENCY_PAIRS[VirtualCurrency.BTC] = arrayOf(
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[VirtualCurrency.BCH] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[Currency.EUR] = arrayOf(
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[Currency.GBP] = arrayOf(
-                    Currency.EUR,
-                    Currency.USD
-            )
-            CURRENCY_PAIRS[VirtualCurrency.ETH] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[VirtualCurrency.LTC] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[VirtualCurrency.XLM] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-            CURRENCY_PAIRS[VirtualCurrency.XRP] = arrayOf(
-                    VirtualCurrency.BTC,
-                    Currency.EUR,
-                    Currency.USD,
-                    Currency.GBP
-            )
-        }
-    }
-
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_CURRENCY_PAIRS
-    }
-
-    override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
-        JSONArray(responseString).forEachJSONObject { market ->
-            if(market.getString("trading") == "Enabled") {
-                val assetsInPair = market.getString("name").split('/')
-                if (assetsInPair.size == 2) {
-                    pairs.add(
-                        CurrencyPairInfo(
-                            assetsInPair[0], // base
-                            assetsInPair[1], // quote
-                            market.getString("url_symbol")
+                    if (assetsInPair.size == 2) {
+                        pairs.add(
+                            CurrencyPairInfo(
+                                assetsInPair[0], // base
+                                assetsInPair[1], // quote
+                                market.getString("url_symbol")
+                            )
                         )
-                    )
+                    }
                 }
             }
-        }
     }
 
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        // Compatibility with old pairs
-        val pairId = checkerInfo.currencyPairId ?: (checkerInfo.currencyBaseLowerCase + checkerInfo.currencyCounterLowerCase)
-        return String.format(URL_TICKER, pairId)
+    override fun getPairId(checkerInfo: CheckerInfo): String {
+        return super.getPairId(checkerInfo) ?: (checkerInfo.currencyBaseLowerCase + checkerInfo.currencyCounterLowerCase)
     }
 
-    @Throws(Exception::class)
-    override fun parseTickerFromJsonObject(requestId: Int, jsonObject: JSONObject, ticker: Ticker, checkerInfo: CheckerInfo) {
+    override fun parseTickerFromJsonObject(
+        requestId: Int,
+        jsonObject: JSONObject,
+        ticker: Ticker,
+        checkerInfo: CheckerInfo
+    ) {
         ticker.bid = jsonObject.getDouble("bid")
         ticker.ask = jsonObject.getDouble("ask")
-        ticker.vol = jsonObject.getDouble("volume")
+
         ticker.high = jsonObject.getDouble("high")
         ticker.low = jsonObject.getDouble("low")
+
+        ticker.vol = jsonObject.getDouble("volume")
         ticker.last = jsonObject.getDouble("last")
         ticker.timestamp = jsonObject.getLong("timestamp")
     }
