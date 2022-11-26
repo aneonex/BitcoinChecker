908fd29a95857a1f8de1d0a1063358267cfb929c
Aneonex Software <69394046+aneonex@users.noreply.github.com>
2022-11-26 10:49:16 +0300

    Updated Gate.io

--- dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/GateIo.kt
+++ dataModule/src/main/java/com/aneonex/bitcoinchecker/datamodule/model/market/GateIo.kt
@@ -3,21 +3,31 @@ package com.aneonex.bitcoinchecker.datamodule.model.market
 import com.aneonex.bitcoinchecker.datamodule.exceptions.MarketParseException
 import com.aneonex.bitcoinchecker.datamodule.model.CheckerInfo
 import com.aneonex.bitcoinchecker.datamodule.model.CurrencyPairInfo
-import com.aneonex.bitcoinchecker.datamodule.model.Market
 import com.aneonex.bitcoinchecker.datamodule.model.Ticker
+import com.aneonex.bitcoinchecker.datamodule.model.market.generic.SimpleMarket
+import com.aneonex.bitcoinchecker.datamodule.util.forEachJSONObject
 import org.json.JSONArray
 
-class GateIo : Market(NAME, TTS_NAME, null) {
-
-    companion object {
-        private const val NAME = "Gate.io"
-        private const val TTS_NAME = "Gate io"
-        private const val URL = "https://api.gateio.ws/api/v4/spot/tickers?currency_pair=%1\$s"
-        private const val URL_CURRENCY_PAIRS = "https://api.gateio.ws/api/v4/spot/currency_pairs"
-    }
-
-    override fun getUrl(requestId: Int, checkerInfo: CheckerInfo): String {
-        return String.format(URL, checkerInfo.currencyPairId)
+class GateIo : SimpleMarket(
+    "Gate.io",
+    "https://api.gateio.ws/api/v4/spot/currency_pairs",
+    "https://api.gateio.ws/api/v4/spot/tickers?currency_pair=%1\$s",
+    "Gate io",
+    errorPropertyName = "message"
+) {
+    override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
+        JSONArray(responseString)
+            .forEachJSONObject { pairJson ->
+                if(pairJson.getString("trade_status") == "tradable") {
+                    pairs.add(
+                        CurrencyPairInfo(
+                            pairJson.getString("base"),
+                            pairJson.getString("quote"),
+                            pairJson.getString("id")
+                        )
+                    )
+                }
+            }
     }
 
     override fun parseTicker(requestId: Int, responseString: String, ticker: Ticker, checkerInfo: CheckerInfo) {
@@ -28,33 +38,12 @@ class GateIo : Market(NAME, TTS_NAME, null) {
 
         ticker.bid = jsonObject.getDouble("highest_bid")
         ticker.ask = jsonObject.getDouble("lowest_ask")
+
         ticker.vol = jsonObject.getDouble("base_volume")
+        ticker.volQuote = jsonObject.getDouble("quote_volume")
+
         ticker.high = jsonObject.getDouble("high_24h")
         ticker.low = jsonObject.getDouble("low_24h")
         ticker.last = jsonObject.getDouble("last")
-    }
-
-    // ====================
-    // Get currency pairs
-    // ====================
-    override fun getCurrencyPairsUrl(requestId: Int): String {
-        return URL_CURRENCY_PAIRS
-    }
-
-    @Throws(Exception::class)
-    override fun parseCurrencyPairs(requestId: Int, responseString: String, pairs: MutableList<CurrencyPairInfo>) {
-        val jsonArray = JSONArray(responseString)
-
-        for (i in 0 until jsonArray.length()) {
-            val pairJson = jsonArray.getJSONObject(i)
-
-            if(pairJson.getString("trade_status") != "tradable") continue
-
-            pairs.add(CurrencyPairInfo(
-                    pairJson.getString("base"),
-                    pairJson.getString("quote"),
-                    pairJson.getString("id")
-            ))
-        }
     }
 }
